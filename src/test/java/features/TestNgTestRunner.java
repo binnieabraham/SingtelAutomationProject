c8d64e2b27192bed666389a0cbf008840433cd5e
binnieabraham <binnieabraham92@gmail.com>
2022-10-31 10:35:45 +0800

    Adding reports functionality

--- src/test/java/features/TestNgTestRunner.java
+++ src/test/java/features/TestNgTestRunner.java
@@ -5,8 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
         features = "src/test/java/features",
-        glue = {"stepDefinitions"}, tags = "@Sanity")
-       //plugin = "json:target/cucumber-reports/CucumberTestReport.json")
+        glue = {"stepDefinitions"}, tags = "@Sanity",
+        plugin ={"pretty","html:target/htmlReports/Automation-report.html","json:target/cucumber.json"},
+        monochrome=true)
 public class TestNgTestRunner extends AbstractTestNGCucumberTests{
 
 }
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,13 +2,18 @@ package stepDefinitions;
 
 import static common.WebDriverVariable.driver;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
@@ -19,9 +24,15 @@ public class Hooks {
         initializeDriver();
     }
     @After()
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName() + "Failure Screenshot");
+        }
         driver.close();
+        driver.quit();
     }
+
 
     private void initializeDriver() throws IOException {
         Properties prop = new Properties();
